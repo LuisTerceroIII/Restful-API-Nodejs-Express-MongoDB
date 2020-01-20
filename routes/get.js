b78a0816e468bf477337c3e1d5bd21bf33055c46
Luis <luis.espinoza.nav@live.com>
2020-01-20 02:17:27 -0300

    Cambios esteticos en todos los meotodos de get.js

--- routes/get.js
+++ routes/get.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../db/db");
 const idValidation = require("./../public/JS/idValidation");
 const mongoose = require("mongoose");
 const ContactDB = require("./../models/contact");
@@ -37,7 +36,7 @@ router.get("/", (req, res) => {
       contacts_json.push(user)
   });
 
-  res.status(200).send(contacts_json);
+  res.status(200).json(contacts_json);
 
  
 });
@@ -48,8 +47,8 @@ router.get("/details", (req, res) => {
 
   ContactDBFull.find({}, (err, contact) => {
     if (err)
-      throw res.status(500).send({
-        error: err
+      throw res.status(500).json({
+        error: err.message
       });
 
     contact.forEach(user => {
@@ -76,47 +75,39 @@ router.get("/details", (req, res) => {
     })
 
     if (contacts_json.length > 1) {
-      res.status(200).send(contacts_json);
+      res.status(200).json(contacts_json);
     } else {
-      res.status(204).send({
+      res.status(204).json({
         message: "Your collection is empty - No content"
       });
     }
   });
 });
 
-router.get("/searchById/:id", (req, res) => {
+router.get("/:id", (req, res) => {
   let id = parseInt(req.params.id, 10);
-  ContactDBFull.find({ "basicInfo.id": id }, async (err, contact) => {
+  ContactDBFull.find({ "id": id }, async (err, contact) => {
     if (err)
-      throw res.status(500).send({
-        status: 500,
-        success: false,
-        message: "We can´t connect right now, try later",
-        error: err
-      });
-    let contacts_json = [
-      { status: "200", success: true, message: "We connect perfectly" }
-    ];
-
+      throw res.status(500).json({error: err.message});
+    let contacts_json = [];
     contact.forEach(contact => {
       let userBasic = {
-        id: contact.basicInfo.id,
-        name: contact.basicInfo.name,
-        lastName: contact.basicInfo.lastName,
-        age: contact.basicInfo.age,
-        email: contact.basicInfo.email
+        id: contact.id,
+        name: contact.name,
+        lastName: contact.lastName,
+        age: contact.age,
+        email: contact.email
       };
 
       contacts_json.push(userBasic);
     });
 
-    if (contacts_json.length !== 1) {
-      res.status(200).send(contacts_json);
+    if (contacts_json.length > 0) {
+      res.status(200).json(contacts_json);
     } else {
       contacts_json.push({ result: "Not found any contact whit that id" });
 
-      res.status(200).send(contacts_json);
+      res.status(404).json(contacts_json);
     }
   });
 });
@@ -142,8 +133,6 @@ router.post("/create", (req, res) => {
         email: req.body.user_email
       }
     );
-    console.log(user.name);
-
     if (
       idValidation.isValid(
         user.name,
@@ -160,6 +149,7 @@ router.post("/create", (req, res) => {
           throw res.status(500).json({error: err});
 
         let contacts_json = [];
+
         contact.forEach(contact => {
           let userFull = {
             id: contact.id,
@@ -173,16 +163,15 @@ router.post("/create", (req, res) => {
         });
         //console.log(util.inspect(contacts_json[2], false, null));
         if (contacts_json.length > 0 ) {
-          res.status(200).send(contacts_json);
+          res.status(200).json(contacts_json);
         } else {
           contacts_json.push({ result: "No content" });
 
-          res.status(204).send(contacts_json);
+          res.status(204).json(contacts_json);
         }
       });
     } else {
-      res.status(200).send({
-        success: false,
+      res.status(200).json({
         message: "Not create - Verify personal information"
       });
     }
@@ -199,9 +188,6 @@ router.post("/", (req, res) => {
   };
 
   const { name = null, lastName, phoneNumber, age, email } = req.body;
-
-  console.log("AGE : " + age);
-  console.log(typeof age);
 
   if (!name) res.status(400).json({ error: "name is require" });
   if (name.length < 3)
@@ -226,126 +212,37 @@ router.post("/", (req, res) => {
     res.status(200).json(contact);
   });
   return null;
-  len().then(result => {
-    let user = new ContactDBFull({
-      basicInfo: {
-        id: result + 1,
-        name: req.body.user_name,
-        lastName: req.body.user_lastName,
-        age: req.body.user_age,
-        email: req.body.user_email
-      },
-
-      infoExtra: {
-        company: req.body.user_company,
-        homepage: req.body.user_homepage,
-
-        /* birthday : req.body.user_birthday, */
-        family: {
-          sister: req.body.user_sister,
-          mom: req.body.user_mom,
-          dad: req.body.user_dad
-        }
-      },
-
-      notes: {
-        note: req.body.user_note
-      }
-    });
-
-    if (
-      idValidation.isValid(
-        user.basicInfo.name,
-        user.basicInfo.lastName,
-        user.basicInfo.age,
-        user.basicInfo.email
-      )
-    ) {
-      user.save((err, userx) => {
-        if (err) throw err;
-      });
-
-      let contacts_json = [
-        {
-          status: "200",
-          success: true,
-          message: "We create a new contact successfuly"
-        }
-      ];
-      let userObject = _.toPlainObject(user);
-
-      let newUser = {
-        basic_Information: {
-          id: userObject.basicInfo.id,
-          name: userObject.basicInfo.name,
-          lastName: userObject.basicInfo.lastName,
-          age: userObject.basicInfo.age,
-          email: userObject.basicInfo.email
-        },
-        extra_Information: {
-          company: userObject.infoExtra.company,
-          homepage: userObject.infoExtra.homepage,
-          family: {
-            sister: userObject.infoExtra.family.sister,
-            brother: userObject.infoExtra.family.brother,
-            mom: userObject.infoExtra.family.mom,
-            dad: userObject.infoExtra.family.dad
-          }
-        },
-        notes: {
-          note: userObject.notes.note
-        }
-      };
-
-      contacts_json.push(newUser);
-      res.send(contacts_json);
-    } else {
-      res.status(200).send({
-        success: false,
-        message: "Not create - Verify personal information"
-      });
-    }
-  });
+ 
 });
 
 //------------------------DELETE------------------------------
 
-router.delete("/deleteById/:id", (req, res) => {
+router.delete("/:id", (req, res) => {
+
   let id = parseInt(req.params.id, 10);
 
   ContactDB.findOneAndDelete({ id: id }, (err, result) => {
     if (err)
-      throw res.status(500).send({
-        status: "500",
-        success: false,
-        message: "We can´t connect right now, try later",
-        error: err
-      });
+      throw res.status(500).json({error: err.message});
 
-    let response_json = [
-      {
-        status: "200",
-        success: true,
-        message: "We delete the choose contact successfuly"
-      }
-    ];
+    let response_json = []
 
     let deleteContact = {
-      id: result.basicInfo.id,
-      name: result.basicInfo.name,
-      lastName: result.basicInfo.lastName,
-      age: result.basicInfo.age,
-      email: result.basicInfo.email
+      id: result.id,
+      name: result.name,
+      lastName: result.lastName,
+      age: result.age,
+      email: result.email
     };
 
     response_json.push(deleteContact);
 
-    if (response_json.length !== 1) {
-      res.status(200).send(response_json);
+    if (response_json.length > 0) {
+      res.status(200).json(response_json);
     } else {
       response_json.push({ result: "Not found any contact whit that id" });
 
-      res.status(200).send(response_json);
+      res.status(204).json(response_json);
     }
   });
 });
@@ -354,184 +251,100 @@ router.delete("/deleteAll", (req, res) => {
   ContactDB.deleteMany({}, (err, result) => {
     res
       .status(200)
-      .send({ message: "jaujauajaua i delete all motherfuckers!" });
+      .json({ message: "jaujauajaua i delete all motherfuckers!" });
   });
 });
 
 //------------------------PUT------------------------------
 
-router.put("/modifyById/:id/email/:email", (req, res) => {
-  //Probar con siguiente logica: Si se envia un -1 en cualquier parametro (no id) no se modifica.
-
-  let id_param = parseInt(req.params.id, 10);
-
-  let email = req.params.email;
-
-  ContactDB.findOneAndUpdate(
-    { id: id_param },
-    { email: email },
-    (err, result) => {
-      if (err)
-        throw res.status(500).send({
-          status: 500,
-          success: false,
-          message: "We can´t connect right now, try later",
-          error: err
-        });
-      let result_json = [
-        { status: 200, success: true, message: "Your updates were successful" }
-      ];
-
-      let updateContact = {
-        id: result.id,
-        name: result.name,
-        lastName: result.lastName,
-        age: result.age,
-        email: result.email
-      };
-
-      result_json.push(updateContact);
-
-      if (result_json.length !== 1) {
-        res.status(200).send(result_json);
-      } else {
-        result_json.push({ result: "Not found any contact whit that id" });
-
-        res.status(200).send(result_json);
-      }
-    }
-  );
-});
-
-router.put("/edit/contact/:id", (req, res) => {
+router.put("/:id", (req, res) => {
   let id = parseInt(req.params.id, 10);
   console.log(req.body.user_name);
 
-  let res_json = [
-    { status: 200, success: true, message: "The edition was complete" }
-  ];
+  let res_json = [];
 
-  ContactDBFull.findOne({ "basicInfo.id": id }, (err, contact) => {
+  ContactDBFull.findOne({ "id": id }, (err, contact) => {
     var editContact = {};
     console.log(contact);
 
     if (err)
-      throw res.status(500).send({
-        status: 500,
-        success: false,
-        message: "We can´t connect right now, try later",
-        error: err
-      });
+      throw res.status(500).json({error: err.message});
 
     if (
       req.body.user_name !== "" &&
-      req.body.user_name !== contact.basicInfo.name
+      req.body.user_name !== contact.name
     ) {
-      editContact.basicInfo.name = req.body.user_name;
+      editContact.name = req.body.user_name;
     }
     if (
       req.body.user_lastName !== "" &&
-      req.body.user_lastName !== contact.basicInfo.lastName
+      req.body.user_lastName !== contact.lastName
     ) {
-      editContact.basicInfo.lastName = req.body.user_lastName;
+      editContact.lastName = req.body.user_lastName;
     }
     if (
       req.body.user_age !== null &&
-      req.body.user_age !== contact.basicInfo.age
+      req.body.user_age !== contact.age
     ) {
-      editContact.basicInfo.age = parseInt(req.body.user_age);
+      editContact.age = parseInt(req.body.user_age);
     }
     if (
       req.body.user_email !== "" &&
-      req.body.user_email !== contact.basicInfo.email
+      req.body.user_email !== contact.email
     ) {
-      editContact.basicInfo.email = req.body.user_email;
+      editContact.email = req.body.user_email;
     }
     if (
       req.body.user_company !== "" &&
-      req.body.user_company !== contact.infoExtra.company
+      req.body.user_company !== contact.company
     ) {
-      editContact.InfoExtra.company = req.body.user_company;
+      editContact.company = req.body.user_company;
     }
     if (
       req.body.user_homepage !== "" &&
-      req.body.user_homepage !== contact.infoExtra.homepage
+      req.body.user_homepage !== contact.homepage
     ) {
-      editContact.InfoExtra.homepage = req.body.user_homepage;
+      editContact.homepage = req.body.user_homepage;
     }
     if (
       req.body.user_sister !== "" &&
-      req.body.user_sister !== contact.infoExtra.family.sister
+      req.body.user_sister !== contact.family.sister
     ) {
-      editContact.InfoExtra.family.sister = req.body.user_sister;
+      editContact.family.sister = req.body.user_sister;
     }
     if (
       req.body.user_mom !== "" &&
-      req.body.user_mom !== contact.infoExtra.family.mom
+      req.body.user_mom !== contact.family.mom
     ) {
-      editContact.InfoExtra.family.mom = req.body.user_mom;
+      editContact.family.mom = req.body.user_mom;
     }
     if (
       req.body_user_dad !== "" &&
-      req.body.user_dad !== contact.infoExtra.family.dad
+      req.body.user_dad !== contact.family.dad
     ) {
-      editContact.InfoExtra.family.dad = req.body.user_dad;
+      editContact.family.dad = req.body.user_dad;
     }
-    if (req.body.user_note != "" && req.body.user_note !== contact.notes.note) {
-      editContact.notes.note = req.body.user_note;
+    if (req.body.user_note != "" && req.body.user_note !== contact.note) {
+      editContact.note = req.body.user_note;
     }
 
-    console.log(editContact);
+    
 
     ContactDBFull.findOneAndUpdate(
-      { "basicInfo.id": id },
+      { "id": id },
       { $set: editContact },
       (err, result) => {
         if (err)
-          throw res.status(500).send({
-            status: 500,
-            success: false,
-            message: "We can not save the changes",
-            error: err
-          });
-
-        console.log(result);
+          throw res.status(500).json({error: err.message});
 
         res_json.push(editContact);
 
-        res.status(200).send(res_json);
+        res.status(200).json(res_json);
       }
     );
   });
 });
 
-//console.log(util.inspect(contacts_json[2], false, null));
-/*  console.log(util.inspect(`Edit contact ${editContact}\n -------------------------------------------------`,false,null))
-    console.log(`En Edit aun: ${JSON.stringify(editContact)}`)
- */
 
-/* ContactDBFull.findOneAndUpdate({'basicInfo.id': id},(err,result) => {
-
-        if(err) throw res.status(500).send({status: 500 ,success: false,message:'We can´t connect right now, try later', error:err})
-
-
-    
-
-        console.log(`---------------------------------------------------------\nResult ${result}`)
-    
-
-
-
-
-
-
-
-           /* res_json.push({msn: 'Personal information update',editContact}) */
-
-/*  res.status(200).send(res_json)
-            
-       
-
-            res.status(500).send({status: 200 ,success: true ,message:'Nothing change', editContact}) */
 
 module.exports = router;
